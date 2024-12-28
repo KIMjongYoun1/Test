e97900f8ab8570fba9ac6a295140b9df74a5811f
ryankim <ryankim@192.168.20.15>
2024-12-28 15:07:25 +0900

    Test

--- src/main/java/Test/config/FilterConfig.java
+++ src/main/java/Test/config/FilterConfig.java
@@ -26,6 +26,7 @@ public class FilterConfig {
 		registartionBean.setFilter(new logInfilter());
 		registartionBean.addUrlPatterns("/*"); // 요청 경로설정
 		registartionBean.setOrder(2); // 필터실행순서 숫자가작을수록 먼저실행
+	
 		return registartionBean;
 		
 	}
